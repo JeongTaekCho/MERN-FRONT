0e0c247814ab848422739ac6a3d6746ae5e445a7
JeongTaekCho <cjt3591@gmail.com>
2023-02-04 15:14:47 +0900

    ✨ Feat :  로그인/회원가입 스위치모드 추가

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NewPlace from "./places/pages/Newplace";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import Auth from "./user/pages/Auth";
 import Users from "./user/pages/Users";
 
 const App = () => {
@@ -25,8 +26,11 @@ const App = () => {
           <Route path="/places/new" exact>
             <NewPlace />
           </Route>
-          <Route path="/places/:placeId" exact>
+          <Route path="/places/:placeId">
             <UpdatePlace />
+          </Route>
+          <Route path="/auth">
+            <Auth />
           </Route>
           <Redirect to="/" />
         </Switch>
---
+++ src/user/pages/Auth.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { useState } from "react";
+import Button from "../../shared/components/FormElements/Button";
+import Input from "../../shared/components/FormElements/Input";
+import Card from "../../shared/components/UIElements/Card";
+import { useForm } from "../../shared/hooks/form-hook";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MAXLENGTH,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+
+import "./Auth.css";
+
+const Auth = () => {
+  const [isLoginMode, setIsLoginMode] = useState(true);
+  const [formState, inputHandle, setFormData] = useForm(
+    {
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  const authSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  const switchModeHandler = () => {
+    if (!isLoginMode) {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: undefined,
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: {
+            value: "",
+            isValid: false,
+          },
+        },
+        false
+      );
+    }
+    setIsLoginMode((prev) => !prev);
+  };
+
+  return (
+    <Card className="authentication">
+      <h2>{isLoginMode ? "Login Required" : "SIGN UP"}</h2>
+      <hr />
+      <form onSubmit={authSubmitHandler}>
+        {!isLoginMode && (
+          <Input
+            element="input"
+            id="name"
+            label="Your Name"
+            type="text"
+            validators={[VALIDATOR_REQUIRE]}
+            errorText="이름을 입력해주세요."
+            onInput={inputHandle}
+          />
+        )}
+        <Input
+          element="input"
+          id="email"
+          type="email"
+          label="E-mail"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="유효한 이메일을 입력해주세요."
+          onInput={inputHandle}
+        />
+        <Input
+          element="input"
+          id="password"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(8)]}
+          errorText="비밀번호를 8자 이상 입력해주세요."
+          onInput={inputHandle}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          {isLoginMode ? "LOGIN" : "SIGNUP"}
+        </Button>
+      </form>
+      <Button inverse onClick={switchModeHandler}>
+        SWITCH TO {isLoginMode ? "LOGIN" : "SIGNUP"}
+      </Button>
+    </Card>
+  );
+};
+
+export default Auth;
